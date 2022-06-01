e0582c6a073294e6691540be4abf376744ca8f49
Romango <homer37@yandex.ru>
2022-06-01 14:07:25 +0400

    hmm

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Content } from '../components/Content/Content';
+import Features from '../components/Features/Features';
+import Hero from '../components/Hero/Hero';
+import { heroOne, heroTwo, heroThree } from '../data/HeroData';
+
+// Hero Feature Content Carousel
+
+const Home = () => {
+	return (
+		<>
+			<Hero />
+			<Features />
+			<Content {...heroOne} />
+			<Content {...heroTwo} />
+			<Content {...heroThree} />
+			
+		</>
+	);
+};
+
+export default Home;
